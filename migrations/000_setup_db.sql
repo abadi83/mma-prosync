75e4a4bf6267d87eb81923d4a61368f44bfbc8b3
abadi83 <abadeereefs@gmail.com>
2026-08-15 16:55:53 +0700

    fix: 000_setup_db.sql idempotent biar aman dijalankan ulang

--- migrations/000_setup_db.sql
+++ migrations/000_setup_db.sql
@@ -1,6 +1,12 @@
--- Setup database dan user untuk MMA ProSync
-CREATE USER mma_admin WITH PASSWORD 'mma_prosync_2024!';
-CREATE DATABASE mma_prosync OWNER mma_admin;
-GRANT ALL PRIVILEGES ON DATABASE mma_prosync TO mma_admin;
-\c mma_prosync
+-- Setup user untuk MMA ProSync (idempotent)
+-- NOTE: Database 'mma_prosync' dibuat manual via psql:
+--   sudo -u postgres psql -c "CREATE DATABASE mma_prosync OWNER mma_admin;"
+-- File ini hanya memastikan user & grants ada.
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'mma_admin') THEN
+    CREATE USER mma_admin WITH PASSWORD 'mma_prosync_2024!';
+  END IF;
+END $$;
+
 GRANT ALL ON SCHEMA public TO mma_admin;
